d22bfb26710ebb09b330356dbb368133152d033a
Schwartz <darlanswtz@gmail.com>
2023-07-20 17:08:30 -0300

    Fix: game create functionality fixed and half of rentals crude implemented

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -14,7 +14,7 @@ export async function getGames(req,res)
 export async function createGame(req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body;
     try {
-        await db.query(`INSERT INTO customers (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)`,[name,image,stockTotal,pricePerDay]);
+        await db.query(`INSERT INTO games ("name", "image", "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,[name,image,stockTotal,pricePerDay]);
         return res.sendStatus(201);
     } catch (error) {
         console.log(error.message);
--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,4 +1,6 @@
-import db from "../database/database.connection.js"
+import dayjs from "dayjs";
+import db from "../database/database.connection.js";
+
 
 export async function getRentals(req,res)
 {
@@ -13,8 +15,21 @@ export async function getRentals(req,res)
 
 export async function createRental(req,res)
 {
+    const {customerId,gameId,daysRented} = req.body;
+    
     try {
-        return res.send('');
+        const customers = await db.query(`SELECT * FROM customers WHERE id=$1;`, [customerId]);
+        if (customers.rowCount === 0) return res.status(400).send("Cliente inexistente!");
+
+        const game = await db.query(`SELECT * FROM games WHERE id=$1;`,[gameId]);
+        if(!game.rows[0]) return res.status(400).send("Jogo inexistente!");
+
+        const pricePerDay  = game.rows[0].pricePerDay;
+
+        await db.query(`INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee") VALUES ($1, $2, $3, $4, $5, null, null);`
+        ,[customerId, gameId, daysRented, dayjs().format('YYYY-MM-DD'), pricePerDay * daysRented]);
+
+        return res.sendStatus(201);
     } catch (error) {
         console.log(error.message);
         return res.status(500).send(error.message);
